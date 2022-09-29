3316240a3b3d8472648b755d353417bba3b493b8
Avery Murray <averylapine@gmail.com>
2022-09-28 17:26:55 -0700

    progress bar
    Signed-off-by: Avery Murray <averylapine@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,10 @@ struct AeiouAssets {
     music: Handle<bevy_kira_audio::AudioSource>,
     #[asset(path = "media.png")]
     keyatlas: Handle<Image>,
+    #[asset(path = "playerball.png")]
+    ball: Handle<Image>,
+    #[asset(path = "line.png")]
+    line: Handle<Image>,
 }
 
 fn main() {
@@ -34,8 +38,8 @@ fn main() {
         })
         .add_loading_state(
             LoadingState::new(AeiouState::Loading)
-            .continue_to_state(AeiouState::Play)
-            .with_collection::<AeiouAssets>()
+                .continue_to_state(AeiouState::Play)
+                .with_collection::<AeiouAssets>(),
         )
         .add_state(AeiouState::Loading)
         .add_plugins_with(DefaultPlugins, |group| {
@@ -44,10 +48,15 @@ fn main() {
         .add_plugin(AudioPlugin)
         // .add_startup_system(setup)
         .add_system_set(
-            SystemSet::on_enter(AeiouState::Play).with_system(play).with_system(setup)
+            SystemSet::on_enter(AeiouState::Play)
+                .with_system(play)
+                .with_system(setup),
         )
         .add_system_set(
-            SystemSet::on_update(AeiouState::Play).with_system(playpause).with_system(bganim)
+            SystemSet::on_update(AeiouState::Play)
+                .with_system(playpause)
+                .with_system(bganim)
+                .with_system(update),
         )
         .add_audio_channel::<Channel>()
         .run();
@@ -61,14 +70,34 @@ struct AeiouTimer(Timer);
 #[derive(Component)]
 struct Media;
 
+#[derive(Component)]
+struct Ball;
+
 struct Ai(Handle<AudioInstance>);
 
-fn playpause(channel: Res<AudioChannel<Channel>>, keys: Res<Input<KeyCode>>, mut instance: ResMut<Ai>, aias: Res<AeiouAssets>, mut query: Query<&mut TextureAtlasSprite, With<Media>>) {
+fn playpause(
+    channel: Res<AudioChannel<Channel>>,
+    keys: Res<Input<KeyCode>>,
+    mut instance: ResMut<Ai>,
+    aias: Res<AeiouAssets>,
+    mut query: Query<&mut TextureAtlasSprite, With<Media>>,
+) {
     if keys.just_pressed(KeyCode::Space) {
         match channel.state(&instance.0) {
-            PlaybackState::Paused { .. } | PlaybackState::Pausing { .. } | PlaybackState::Queued => { set_media(&mut query, AeiouMedia::Play); channel.resume(); }
-            PlaybackState::Playing { .. } => { set_media(&mut query, AeiouMedia::Paused); channel.pause(); }
-            PlaybackState::Stopped | PlaybackState::Stopping { .. } => { set_media(&mut query, AeiouMedia::Play); instance.0 = channel.play(aias.music.clone()).handle(); }
+            PlaybackState::Paused { .. }
+            | PlaybackState::Pausing { .. }
+            | PlaybackState::Queued => {
+                set_media(&mut query, AeiouMedia::Play);
+                channel.resume();
+            }
+            PlaybackState::Playing { .. } => {
+                set_media(&mut query, AeiouMedia::Paused);
+                channel.pause();
+            }
+            PlaybackState::Stopped | PlaybackState::Stopping { .. } => {
+                set_media(&mut query, AeiouMedia::Play);
+                instance.0 = channel.play(aias.music.clone()).handle();
+            }
         }
     }
 }
@@ -86,12 +115,6 @@ fn set_media(query: &mut Query<&mut TextureAtlasSprite, With<Media>>, state: Aei
     }
 }
 
-// fn setup_media(mut query: Query<&mut TextureAtlasSprite, With<Media>>) {
-//     for mut image in &mut query {
-//         image.index = 1;
-//     }
-// }
-
 fn bganim(time: Res<Time>, mut query: Query<(&mut AeiouTimer, &mut TextureAtlasSprite)>) {
     for (mut tmr, mut atl) in &mut query {
         if tmr.0.tick(time.delta()).just_finished() {
@@ -106,25 +129,80 @@ fn bganim(time: Res<Time>, mut query: Query<(&mut AeiouTimer, &mut TextureAtlasS
     }
 }
 
-fn play(mut cmds: Commands, channel: Res<AudioChannel<Channel>>, aias: Res<AeiouAssets>, mut query: Query<&mut TextureAtlasSprite, With<Media>>) {
+fn play(
+    mut cmds: Commands,
+    channel: Res<AudioChannel<Channel>>,
+    aias: Res<AeiouAssets>,
+) {
     cmds.insert_resource(Ai(channel.play(aias.music.clone()).handle()));
+}
+
+//  1824
+fn update(mut query: Query<&mut Transform, With<Ball>>, channel: Res<AudioChannel<Channel>>, instance: Res<Ai>) {
+    if let Some(pc) = channel.state(&instance.0).position().map(|f| (f as f32) / 1824.) {
+        let pos = (482. * (pc / 100.)) - 233.;
+        for mut trans in &mut query {
+            trans.translation.x = pos as f32;
+        }
+    }
 }
 
 fn setup(mut cmds: Commands, aias: Res<AeiouAssets>, mut textures: ResMut<Assets<TextureAtlas>>) {
     cmds.spawn_bundle(SpriteSheetBundle {
-        texture_atlas: textures.add(TextureAtlas::from_grid(aias.atlas.clone(), Vec2::splat(498.0), 7, 9)),
+        texture_atlas: textures.add(TextureAtlas::from_grid(
+            aias.atlas.clone(),
+            Vec2::splat(498.0),
+            7,
+            9,
+        )),
         ..default()
-    }).insert(AeiouTimer(Timer::new(Duration::from_millis(100), true)));
+    })
+    .insert(AeiouTimer(Timer::new(Duration::from_millis(100), true)));
 
     cmds.spawn_bundle(SpriteSheetBundle {
-        texture_atlas: textures.add(TextureAtlas::from_grid(aias.keyatlas.clone(), Vec2::splat(64.0), 2, 1)),
-        transform: Transform { translation: Vec3 { x: 0.0, y: -166.0, z: 1.0 }, ..default() },
+        texture_atlas: textures.add(TextureAtlas::from_grid(
+            aias.keyatlas.clone(),
+            Vec2::splat(64.0),
+            2,
+            1,
+        )),
+        transform: Transform {
+            translation: Vec3 {
+                x: 0.0,
+                y: -166.0,
+                z: 1.0,
+            },
+            ..default()
+        },
         sprite: TextureAtlasSprite {
             index: 1,
             ..default()
         },
         ..default()
-    }).insert(Media);
+    })
+    .insert(Media);
+
+    cmds.spawn_bundle(
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(-164.0, -233.0, 1.0),
+                ..default()
+            },
+            texture: aias.ball.clone(),
+            ..default()
+        }
+    ).insert(Ball);
+
+    cmds.spawn_bundle(
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0., -233.0, 1.0),
+                ..default()
+            },
+            texture: aias.line.clone(),
+            ..default()
+        }
+    );
 
     cmds.spawn_bundle(Camera2dBundle::default());
 }
